31814a196347bbb984a3f72ba858043e6ff6a9ad
Ceo-Sammarco <noah.kd@gmail.com>
2026-03-29 23:43:41 -0300

    update:  M themes.js

--- themes.js
+++ themes.js
@@ -1,6 +1,13 @@
 /**
  * --- themes --- */
 export const themes = {
+  catpuccin: {
+    id: "catpuccin",
+    c0: "#151d27",
+    c1: "#1d2633",
+    c2: "#252f3d",
+    text: "#fff",
+  },
   rosePine: {
     id: "rosé-pine",
     c0: "#191724",
